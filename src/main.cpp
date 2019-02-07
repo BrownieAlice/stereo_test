b5bb61af39d19d6066f2059972375a0115ba48c8
BrownieAlice <info@browniealice.net>
2019-02-07 22:19:12 +0900

    moidfy

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,23 @@
 #include <opencv2/opencv.hpp>
 #include <cmath>
 
-constexpr int minDisparity = 32;
-constexpr int numDisparities = 112;
-constexpr int SADWindowSize = 16;
-constexpr int P1 = 8 * 3 * 5;
-constexpr int P2 = 32 * 3 * 5;
-constexpr int disp12MaxDiff = 20;
-constexpr int preFilterCap = 0;
-constexpr int uniquenessRatio = 10;
-constexpr int speckleWindowSize = 100;
-constexpr int speckleRange = 32;
-constexpr bool fullDp = true;
+constexpr int minDisparity = 0;
+constexpr int numDisparities = 16 * 8;
+constexpr int SADWindowSize = 11;
+constexpr int P1 = 8 * 3 * SADWindowSize * SADWindowSize;
+constexpr int P2 = 32 * 3 * SADWindowSize * SADWindowSize;
+constexpr int disp12MaxDiff = 240;
+constexpr int preFilterCap = 10;
+constexpr int uniquenessRatio = 15;
+constexpr int speckleWindowSize = 200;
+constexpr int speckleRange = 2;
+constexpr bool fullDp = false;
 cv::Ptr<cv::StereoSGBM> ssgbm = cv::StereoSGBM::create(
   minDisparity, numDisparities, SADWindowSize, P1, P2,
   disp12MaxDiff, preFilterCap, uniquenessRatio,
   speckleWindowSize, speckleRange, fullDp);
+
+constexpr size_t gaussian_size = 5;
 
 int main() {
   cv::VideoCapture cap1(0);
@@ -68,19 +70,25 @@ int main() {
     cv::imshow("Camera Right", frame2);
     //画像を表示．
 
-    cv::cvtColor(frame1, frame1, cv::COLOR_BGR2GRAY);
-    cv::equalizeHist(frame1, frame1);
-    cv::GaussianBlur(frame1, frame1, cv::Size(5, 5), 0);
-    cv::cvtColor(frame2, frame2, cv::COLOR_BGR2GRAY);
-    cv::equalizeHist(frame2, frame2);
-    cv::GaussianBlur(frame2, frame2, cv::Size(5, 5), 0);
+    //  cv::cvtColor(frame1, frame1, cv::COLOR_BGR2GRAY);
+    //  cv::equalizeHist(frame1, frame1);
+    cv::GaussianBlur(frame1, frame1,
+      cv::Size(gaussian_size, gaussian_size), 0);
+    //  cv::cvtColor(frame2, frame2, cv::COLOR_BGR2GRAY);
+    //  cv::equalizeHist(frame2, frame2);
+    cv::GaussianBlur(frame2, frame2,
+      cv::Size(gaussian_size, gaussian_size), 0);
 
     ssgbm->compute(frame1, frame2, calc_depth);
     double min, max;
     cv::minMaxLoc(calc_depth, &min, &max);
+    std::cout << "min: " << min << ", max: " << max << std::endl;
+    min = -16;
+    max = 1008;
     calc_depth.convertTo(depth, CV_8UC1, 255.0 / (max - min),
       -255.0 * min / (max - min));
     cv::imshow("result", depth);
+
 
     cv::waitKey(1);
   }
